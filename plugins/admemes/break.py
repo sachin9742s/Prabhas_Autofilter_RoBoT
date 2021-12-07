26e3dcf5eacb5c3bb5f2e388d2ed3ffe29570488
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-12-07 17:30:56 +0530

    Add files via upload

--- plugins/admemes/break.py
+++ plugins/admemes/break.py
@@ -1,26 +1,24 @@
-
-
 from pyrogram import Client, filters
 from info import COMMAND_HAND_LER
 from plugins.helper_functions.cust_p_filters import f_onw_fliter
 
-# LUCK------------ https://telegram.me/Josprojects ------------ #
+
 
 # EMOJI CONSTANTS
-TRY_YOUR_LUCK = "🎰"
+BREAK_YOUR_LOVE = "💔"
 # EMOJI CONSTANTS
 
 @Client.on_message(
-    filters.command(["luck", "cownd"])
+    filters.command(["break", "thepp"])
 )
-async def luck_cownd(client, message):
-    """ /luck an @animatedluck """
+async def break_thepp(client, message):
+    """ /break an @animatedbreak """
     rep_mesg_id = message.message_id
     if message.reply_to_message:
         rep_mesg_id = message.reply_to_message.message_id
     await client.send_dice(
         chat_id=message.chat.id,
-        emoji=TRY_YOUR_LUCK,
+        emoji=BREAK_YOUR_LOVE,
         disable_notification=True,
         reply_to_message_id=rep_mesg_id
     )
