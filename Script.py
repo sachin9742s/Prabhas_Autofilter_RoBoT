f25123710c0c6681987e144f6d7a0b0981de04da
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2022-05-29 17:34:49 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -9,8 +9,8 @@ class script(object):
 ✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵
 ╔════❰ ꪖ᥇ꪮꪊ𝓽 ꪑ𝘴ᧁ ❱═❍⊱❁۪۪
 ║╭━━━━━━━━━━━━━━━➣ 
-║┣⪼ 𝙈𝙔 𝙉𝘼𝙈𝙀 - <a href="https://t.me/Dqautofl_Bot"> 🅿🆁🅰🅱🅷🅰🆂 🅁🄾🄱🄾🅃 </a>
-║┣⪼ Ⓓ︎Ⓔ︎Ⓥ︎ - <a href="https://t.me/sachin_official_admin"> 🅢🅐🅒🅗🅘🅝 Ⓢ </a>
+║┣⪼ 𝙈𝙔 𝙉𝘼𝙈𝙀 - <a href="https://t.me/Dqautofl_Bot"> Prabhas</a>
+║┣⪼ Ⓓ︎Ⓔ︎Ⓥ︎ - <a href="https://t.me/sachin_official_admin"> Sachin S</a>
 ║┣⪼ 𝓛𝓲𝓫𝓻𝓪𝓻𝓻𝔂 - 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ║┣⪼ 𝓛𝓪𝓷𝓰𝓾𝓪𝓰𝓮 - 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ║┣⪼ 𝓓𝓪𝓽𝓪 𝓑𝓪𝓼𝓮 - 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
