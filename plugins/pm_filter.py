08ffa023b022f2700063f79e2b22d1c765e91779
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2022-06-02 15:47:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -164,6 +164,7 @@ async def next_page(bot, query):
                 InlineKeyboardButton(f"🗓 {round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages"),
                 InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{req}_{key}_{n_offset}")
             ],
+        )
             [
         btn.append(
                 InlineKeyboardButton(text="🤖 CHECK MY PM 🤖", url=f"https://telegram.dog/Prabhas_autofilterBOT")
