18f2cf1f837b1b3103f69654dfca97f0b32707b0
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2022-06-11 11:54:21 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,7 +1,7 @@
 class script(object):
     START_TXT = """<b>Hello 👋 {}. 𝖭𝗂𝖼𝖾 𝗍𝗈 𝗆𝖾𝖾𝗍 𝗒𝗈𝗎 🙌</b>
 
-<b>I'm 𝗣𝗿𝗮𝗯𝗵𝗮𝘀 or You Can Call me as [𝗣𝗿𝗮𝗯𝗵𝗮𝘀 𝗔𝘂𝘁𝗼𝗳𝗶𝗹𝘁𝗲𝗿 𝗯𝗼𝘁](t.me/Prabhas_autofilterBOT) 😍.</b>
+<b>I'm 𝗣𝗿𝗮𝗯𝗵𝗮𝘀 or You Can Call me as  <a href="https://t.me/Prabhas_autofilterBOT"> 𝗣𝗿𝗮𝗯𝗵𝗮𝘀 𝗔𝘂𝘁𝗼𝗳𝗶𝗹𝘁𝗲𝗿 𝗯𝗼𝘁</a> 😍.</b>
 
 <b>I Can Provide Movies In Your Group, It Very Easy Way Just Add Me To Your Group And Make Me Admin In Your Group, Thars all I'll Provide Movies From Your Group.</b>
 
