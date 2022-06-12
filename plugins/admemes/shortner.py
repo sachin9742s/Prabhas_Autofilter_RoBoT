4f949ca570821bfcdaec05941880170b24a1f1f9
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2022-06-12 22:53:32 +0530

    Update shortner.py

--- plugins/admemes/shortner.py
+++ plugins/admemes/shortner.py
@@ -15,7 +15,7 @@ OWLY_API = os.environ.get("OWLY_API", None)
 BUTTONS = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text='⚙ ᴅᴋ ʙᴏᴛx ⚙', url='https://t.me/dk_botx')
+            InlineKeyboardButton(text='⚙ support ⚙', url='https://t.me/Kiccharequest')
         ]
     ]
 )
